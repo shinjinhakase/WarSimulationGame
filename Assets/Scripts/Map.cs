9c962850db31fb5612e6eceeb3c0dbec319f3bfb
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-03-10 23:56:41 +0900

    マップデータを作成

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour{
+
+    string mapData = new String[6,11];
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(var data in mapData)
+        {
+            data = "grass";    
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
