d9e5e5b6f5bde6429cb180d6c2f091e8a260370b
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-03-11 00:34:29 +0900

    クリックした座標を表示

---
+++ Assets/Scripts/TestTouchEvent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTouchEvent : MonoBehaviour
+{
+    
+    Vector2 touchPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        touchPoint = new Vector2();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            touchPoint = Input.mousePosition;
+            Debug.Log(touchPoint.x + "," + touchPoint.y);
+        }
+    }
+}
