071a77513afa6471c88952d3e51119ed04861e29
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-03-11 11:26:08 +0900

    駒がクリックされた場合、選択状態となりほかの駒は選択できないようにする

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,27 +6,34 @@ using UnityEngine.Tilemaps;
 public class Move : MonoBehaviour
 {
     
-    Vector3 touchPoint;
+    bool isSelected;
     [SerializeField] Tilemap map;
     [SerializeField] Tilemap charaSheet;
     
     // Start is called before the first frame update
     void Start()
     {
-        touchPoint = new Vector3();
+        isSelected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3 touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var getTile = map.WorldToCell(touchPoint);
             
             var selectedChara = charaSheet.GetTile(new Vector3Int(getTile.x,getTile.y,0));
-            if(selectedChara != null){
+            if(selectedChara == null){
+                isSelected = false;
+            }else if(isSelected == false){
                 Debug.Log("選択された");
+                isSelected = true;
             }
+
         }
+
     }
+
 }
