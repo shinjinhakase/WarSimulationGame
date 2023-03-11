f978b86ff8b4af0d853f5a21f576678abc036e37
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-03-11 15:02:19 +0900

    スクリプトで表示処理を実装

--- Assets/Scripts/DrawMovableTile.cs
+++ Assets/Scripts/DrawMovableTile.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+
 public class DrawMovableTile : MonoBehaviour
 {
+    [SerializeField] Tilemap movableMap;
     [SerializeField] Tile movableTile;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void DrawTile(int x,int y){
+
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                movableMap.SetTile(new Vector3Int(i,j,0),movableTile);
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void DeleteTile(int x,int y){
+
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                movableMap.SetTile(new Vector3Int(i,j,0),null);
+            }
+        }
+
     }
+
 }
