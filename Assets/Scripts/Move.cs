f3e5b9637e9fd89f011ff401753ab6f43f74b778
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-03-11 12:12:18 +0900

    選択状態のリファクタリング

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,14 +6,13 @@ using UnityEngine.Tilemaps;
 public class Move : MonoBehaviour
 {
     
-    bool isSelected;
+    bool isSelected　= false;
     [SerializeField] Tilemap map;
     [SerializeField] Tilemap charaSheet;
     
     // Start is called before the first frame update
     void Start()
     {
-        isSelected = false;
     }
 
     // Update is called once per frame
@@ -22,16 +21,14 @@ public class Move : MonoBehaviour
         if(Input.GetMouseButtonDown(0)){
 
             Vector3 touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var getTile = map.WorldToCell(touchPoint);
+            var touchPointCell = map.WorldToCell(touchPoint);
             
-            var selectedChara = charaSheet.GetTile(new Vector3Int(getTile.x,getTile.y,0));
-            if(selectedChara == null){
-                if(isSelected == true){
-                    Debug.Log("移動先" + getTile.x + "," + getTile.y);
-                }
-                isSelected = false;
-            }else if(isSelected == false){
+            if(charaSheet.HasTile(new Vector3Int(touchPointCell.x,touchPointCell.y,0))){
                 isSelected = true;
+                Debug.Log("選択された");
+            }else{
+                isSelected = false;
+                Debug.Log("選択解除");
             }
 
         }
