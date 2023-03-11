8106902df5ff4f53a5a45732901989c76d199690
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-03-11 17:19:53 +0900

    カーソルを生成

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class Cursor : MonoBehaviour
 {
+    public Vector3Int init;
+    
+    [SerializeField] Tilemap cursorMap;
+    [SerializeField] Tile cursorTile;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cursorMap.SetTile(init,cursorTile);
     }
 
     // Update is called once per frame
