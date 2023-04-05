457cd1509f7b686b6a3a241b0d0eeadffe35ba80
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-04-05 12:44:11 +0900

    ユニット作成時にScriptableObjectを参照

--- Assets/Scripts/UnitStatus.cs
+++ Assets/Scripts/UnitStatus.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "MyScriptable/Create UnitStatus")]
 public class UnitStatus : ScriptableObject
 {
+    public string team;
     public int minHP;
     public int maxHP;
     public int minATK;
