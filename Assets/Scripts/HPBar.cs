2994bfbc82a48db960a8fa365ff947167ac95835
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2023-04-25 00:28:28 +0900

    ユニットの体力を参照して表示を変える

--- Assets/Scripts/HPBar.cs
+++ Assets/Scripts/HPBar.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class HPBar : MonoBehaviour{
     
     Unit myUnit;
+    Slider bar;
 
     public void setUnit(Unit unit){
         this.myUnit = unit;
+        bar = this.GetComponent<Slider>();
+        bar.maxValue = myUnit.getMaxHP();
     }
 
     void Update(){
         if(myUnit != null){
             RectTransform position = this.GetComponent<RectTransform>();
             position.anchoredPosition = new Vector2(myUnit.getPosition().x,myUnit.getPosition().y);
+            bar.value = myUnit.getHP();
+        }
+        if(myUnit.getHP() < 0){
+            Destroy(this.gameObject);
         }
     }
 
